fcd954e34eba65aa170b0564d92faddabda19047
Yeser Amer <yamer@redhat.com>
2021-05-07 17:20:00 +0200

    Scesim Junit5

--- dmn-quarkus-example/src/test/java/testscenario/KogitoScenarioJunitActivatorTest.java
+++ dmn-quarkus-example/src/test/java/testscenario/KogitoScenarioJunitActivatorTest.java
@@ -15,12 +15,15 @@
  */
 package testscenario;
 
+import org.junit.platform.commons.annotation.Testable;
+
 /**
  * KogitoJunitActivator is a custom JUnit runner that enables the execution of Test Scenario files (*.scesim).
  * This activator class, when executed, will load all scesim files available in the project and run them.
  * Each row of the scenario will generate a test JUnit result.
  */
 //@org.junit.runner.RunWith(org.kogito.scenariosimulation.runner.KogitoJunitActivator.class)
+@Testable
 public class KogitoScenarioJunitActivatorTest {
 
 }
